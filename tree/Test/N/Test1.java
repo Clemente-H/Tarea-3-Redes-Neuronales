469f13830e5f1a04f18512d457bd800bc3897734
jcitog <jcarayav625@gmail.com>
2021-01-05 21:45:39 -0300

    jar graphips + class ventana

--- tree/Test/N/Test1.java
+++ tree/Test/N/Test1.java
@@ -25,12 +25,12 @@ public class Test1 {
     int n_tournament;
     int n_hijos_gen;
     int ult_generacion;
-    int res=0;
+    int res = 0;
     int generacion = 1;
 
 
     @Test
-    public void Test() {
+    public void Test11() {
         expresion = "1 2 3 4 5 6 7 8 9";
         mutacion_prob = 0.5;
         goal = 7;
@@ -41,29 +41,29 @@ public class Test1 {
 
         String[] numero = expresion.split(" ");
         int[] N_arr = new int[numero.length];
-        for (int i = 0; i <numero.length ; i++){
-            N_arr[i]=Integer.parseInt(numero[i]);
+        for (int i = 0; i < numero.length; i++) {
+            N_arr[i] = Integer.parseInt(numero[i]);
         }
         List<Nodo> nodos = generate_Poblacion(tamanoPoblacion, N_arr, h_m_ax);
-        while( res!=goal && generacion<ult_generacion){
-
-        for(int k = 0; k<n_hijos_gen;k++){
-            int randomNum1 = ThreadLocalRandom.current().nextInt(1, tamanoPoblacion);
-            nodos.remove(randomNum1);
-            Nodo Hijo = tournamet_selection(nodos,goal,n_tournament);
-            nodos.add(Hijo);
-        }
-        for(int t = 0; t<tamanoPoblacion;t++){
-            int fit_c_u = fitness(nodos.get(t),goal);
-            if(fit_c_u == 0){
-                String e  = print(nodos.get(t));
-                System.out.print(e);
-                System.out.print("\n");
-                String n = "encontrado en la generacion" + generacion;
-                System.out.print(n);
-                break;
+        while (res != goal && generacion < ult_generacion) {
+            for (int k = 0; k < n_hijos_gen; k++) {
+                int randomNum1 = ThreadLocalRandom.current().nextInt(1, tamanoPoblacion);
+                nodos.remove(randomNum1);
+                Nodo Hijo = tournamet_selection(nodos, goal, n_tournament);
+                nodos.add(Hijo);
             }
+            for (int t = 0; t < tamanoPoblacion; t++) {
+                int fit_c_u = fitness(nodos.get(t), goal);
+                if (fit_c_u == 0) {
+                    String e = print(nodos.get(t));
+                    System.out.print(e);
+                    System.out.print("\n");
+                    String n = "encontrado en la generacion" + generacion;
+                    System.out.print(n);
+                    break;
+                }
+            }
+            generacion++;
         }
-        generacion++;
-
-        }
+    }
+}
